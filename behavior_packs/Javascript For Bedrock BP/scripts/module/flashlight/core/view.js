48adbce0e76f9a8025bcbefd6fe77a7a7407d855
SolightzZ <reimdes1@gmail.com>
2026-07-31 13:01:51 +0700

    fix: Dynamic Light y+1

--- behavior_packs/Javascript For Bedrock BP/scripts/module/flashlight/core/view.js
+++ behavior_packs/Javascript For Bedrock BP/scripts/module/flashlight/core/view.js
@@ -14,7 +14,7 @@ function getDimBounds(dimension) {
 
 export function calcLightPos(pos, dimension) {
    const b = getDimBounds(dimension);
-   const y = Math.floor(pos.y);
+   const y = Math.floor(pos.y) + 1;
    if (y < b.min || y > b.max) return null;
    return {
       x: Math.floor(pos.x),
